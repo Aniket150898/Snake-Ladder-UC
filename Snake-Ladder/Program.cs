b13af1b65a85663a499f632214853eccf367f5b5
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-08 20:06:31 +0530

    First

---
+++ Snake-Ladder/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake_Ladder
+{
+    public class SnakeAndLadderProgram
+    {
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.SnakeAndLadder();
+        }
+    }
+}
