bef04f433067786d7b71490b0f87e384e43c7e57
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-09 14:38:49 +0530

    AllFiles

---
+++ Snake-Ladder/DiceRolls.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    public class Program
+    {
+        public void DiceRolls()
+        {
+
+            Random diceroll = new Random();
+            int dice = diceroll.Next(1, 7);
+            Console.WriteLine(" Player roll the dice and the number is :" + dice);
+
+        }
+    }
+}
---
+++ Snake-Ladder/SnakeAndLadder.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Snake_Ladder
+//{
+//    public class Program
+//    {
+//        public int Starting_Position = 0;
+//        public void SnakeAndLadder()
+//        {
+
+//            Console.WriteLine(" Welcome to the Snake And Ladder Game ");
+//            Console.WriteLine(" Your Initial Position is: " + Starting_Position);
+
+//        }
+//    }
+//}
